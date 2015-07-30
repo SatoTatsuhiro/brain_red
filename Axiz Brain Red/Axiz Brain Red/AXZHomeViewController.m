6c0ffb86a5897be2ccecd1a670a907ce23dd1f1f
tatsuleft <tatsuleft@satoujuyou-no-MacBook-Pro.local>
2015-07-31 02:28:13 +0900

    set meterViews

--- Axiz Brain Red/Axiz Brain Red/AXZHomeViewController.m
+++ Axiz Brain Red/Axiz Brain Red/AXZHomeViewController.m
@@ -18,11 +18,18 @@
     [self _initialize];
 }
 
-
 - (void)_initialize
 {
     [self backgroundImages];
-    [self meterViews];
+    [self _prepareMeterViews];
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * PAGE_COUNT);
+    [self.view addSubview:scrollView];
+    
+    for (AXZMeterView *meterView in self.meterViews) {
+        [scrollView addSubview:meterView];
+    }
 }
 
 - (NSMutableArray *)backgroundImages
@@ -30,7 +37,7 @@
     NSMutableArray *backgroundImages = [[NSMutableArray alloc] init];
     
     [backgroundImages addObject:[UIImage imageNamed:@"red_background_4"]];
-    [backgroundImages addObject:[UIImage imageNamed:@"red_background_4"]];
+    [backgroundImages addObject:[UIImage imageNamed:@"inner_circle_km_red"]];
     [backgroundImages addObject:[UIImage imageNamed:@"red_background_4"]];
     [backgroundImages addObject:[UIImage imageNamed:@"red_background_4"]];
     [backgroundImages addObject:[UIImage imageNamed:@"red_background_4"]];
@@ -38,21 +45,19 @@
     return backgroundImages;
 }
 
-- (NSMutableArray *)meterViews
+- (void)_prepareMeterViews
 {
-    NSMutableArray *meterviews = [[NSMutableArray alloc] init];
+    self.meterViews = [[NSMutableArray alloc] init];
     
     for (int i = 0; i < PAGE_COUNT; i++) {
         UINib *nib = [UINib nibWithNibName:@"AXZMeterView" bundle:nil];
         AXZMeterView *meterView = [nib instantiateWithOwner:self options:nil][0];
         meterView.backgroundImageView.image = self.backgroundImages[i];
-        [meterviews addObject:meterView];
+        meterView.frame = CGRectMake(0, self.view.frame.size.height * i, self.view.frame.size.width, self.view.frame.size.height);
+        [self.meterViews addObject:meterView];
     }
     
-    return meterviews;
 }
-
-
 //=============================================================
 #pragma AXZMeterViewDelegate
 //=============================================================
