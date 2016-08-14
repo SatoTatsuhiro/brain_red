ce3e7f8ba9bc32ccbc9aa3c57fe68ab0512e10b2
tatsuhiro-sato <tatsuhiro-sato@cookpad.com>
2016-08-14 14:47:49 +0900

    Fix iPhone6+ main label

--- Axiz Brain Red/Axiz Brain Red/UIView+AXZUI.m
+++ Axiz Brain Red/Axiz Brain Red/UIView+AXZUI.m
@@ -65,7 +65,7 @@
     } else if (screenRect.size.width == 667 && screenRect.size.height == 375) {
         return CGRectMake(107, 233, 52, 21);
     } else if (screenRect.size.width == 736 && screenRect.size.height == 414) {
-        return CGRectMake(113, 257, 42, 21);
+        return CGRectMake(103, 257, 62, 21);
     }
     return CGRectMake(0, 0, 0, 0);
 }
@@ -81,7 +81,7 @@
     } else if (screenRect.size.width == 667 && screenRect.size.height == 375) {
         return CGRectMake(509, 233, 52, 21);
     } else if (screenRect.size.width == 736 && screenRect.size.height == 414) {
-        return CGRectMake(583, 257, 42, 21);
+        return CGRectMake(573, 257, 62, 21);
     }
     return CGRectMake(0, 0, 0, 0);
 }
